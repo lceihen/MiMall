cdb27d8744ce4d9028956f05576c9c76ef7ec45d
lceihen <2750418124@qq.com>
2020-11-18 22:14:28 +0800

    axios、jsonp的使用

--- mimall/src/App.vue
+++ mimall/src/App.vue
@@ -8,6 +8,7 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import axios from 'axios';
+import jsonp from 'jsonp';
 export default {
   name: 'App',
   components: {
@@ -15,8 +16,11 @@ export default {
   },
   mounted(){
     let url="https://www.easy-mock.com/mock/5b012c1fe6e1035843cd3aff/mockapi/table/list";
-    axios.get(url).then(()=>{
-      
+    axios.get(url).then((data)=>{
+console.log("axios------",data);
+    })
+    jsonp(url,(error,data)=>{
+console.log("jsonp------",data);
     })
   }
 }
