689b7f31e1621d957f0d17663f9663f72c5d24d0
lceihen <2750418124@qq.com>
2020-11-24 21:05:06 +0800

    接口介绍完毕

--- mimall/src/App.vue
+++ mimall/src/App.vue
@@ -8,13 +8,35 @@
 
 
 //import jsonp from 'jsonp';
+//import storage from './storage/index';
+import axios from 'axios';
 export default {
   name: 'App',
   components: {
    
   },
+  data(){
+    return {
+      res:{}
+    }
+  },
   mounted(){
-
+//storage.setItem('userinner',{a:1},"user");
+//storage.setItem('b',{'a':1});
+//storage.clear('a','user');
+// axios.get('./mock/user/login.json').then((res)=>{
+//   this.res=res
+//   this.getaxios();
+// });
+axios.get('/user/login').then((res)=>{
+  this.res=res
+  this.getaxios();
+});
+  },
+  methods:{
+    getaxios(){
+console.log(this.res)
+    }
   }
 }
 </script>
