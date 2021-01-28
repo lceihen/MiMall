ba77a4a05b98f00ab4b0b672cea07069ca8880ab
lceihen <2750418124@qq.com>
2021-01-28 18:34:18 +0800

    路由按需加载1

--- mimall/src/router.js
+++ mimall/src/router.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './pages/home'
-import Login from './pages/login'
+//import Login from './pages/login'
 import Index from './pages/index';
-import Product from './pages/product';
-import Detail from './pages/detail';
+//import Product from './pages/product';
+//import Detail from './pages/detail';
 import Cart from './pages/cart';
 import Order from './pages/order';
 import OrderConfirm from './pages/orderConfirm';
@@ -13,14 +13,12 @@ import OrderPay from './pages/orderPay';
 import AliPay from './pages/alipay';
 Vue.use(Router);
 export default new Router({
-    routes: [
-        {
+    routes: [{
             path: '/',
             name: 'home',
             component: Home,
-            redirect:'/index',
-            children: [
-                {
+            redirect: '/index',
+            children: [{
                     path: 'index',
                     name: 'index',
                     component: Index
@@ -28,12 +26,14 @@ export default new Router({
                 {
                     path: 'product/:id',
                     name: 'product',
-                    component: Product
+                    //    component: Product,
+                    component: resolve => require(['./pages/product.vue', resolve])
                 },
                 {
                     path: 'detail/:id',
                     name: 'detail',
-                    component: Detail
+                    //  component: Detail
+                    component: resolve => require(['./pages/detail.vue', resolve])
                 },
             ]
         },
@@ -44,16 +44,16 @@ export default new Router({
             component: Cart,
         },
         {
-path:'/login',
-name:'login',
-component:Login
+            path: '/login',
+            name: 'login',
+            //component: Login
+            component: resolve => require(['./pages/login.vue'], resolve)
         },
         {
             path: '/order',
             name: 'order',
             component: Order,
-            children: [
-                {
+            children: [{
                     path: 'list',
                     name: 'order-list',
                     component: OrderList,
@@ -69,9 +69,9 @@ component:Login
                     component: OrderPay,
                 },
                 {
-                    path:'alipay',
-                    name:'alipay',
-                    component:AliPay,
+                    path: 'alipay',
+                    name: 'alipay',
+                    component: AliPay,
                 }
             ]
         }
