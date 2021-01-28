2c970c8062c5167ae3f16912562f7d1c79787cef
lceihen <2750418124@qq.com>
2021-01-28 19:06:28 +0800

    真正按需加载

--- mimall/src/router.js
+++ mimall/src/router.js
@@ -5,12 +5,12 @@ import Home from './pages/home'
 import Index from './pages/index';
 //import Product from './pages/product';
 //import Detail from './pages/detail';
-import Cart from './pages/cart';
-import Order from './pages/order';
-import OrderConfirm from './pages/orderConfirm';
-import OrderList from './pages/orderList';
-import OrderPay from './pages/orderPay';
-import AliPay from './pages/alipay';
+//import Cart from './pages/cart';
+//import Order from './pages/order';
+//import OrderConfirm from './pages/orderConfirm';
+//import OrderList from './pages/orderList';
+//import OrderPay from './pages/orderPay';
+//import AliPay from './pages/alipay';
 Vue.use(Router);
 export default new Router({
     routes: [{
@@ -27,13 +27,17 @@ export default new Router({
                     path: 'product/:id',
                     name: 'product',
                     //    component: Product,
-                    component: resolve => require(['./pages/product.vue', resolve])
+                    //  component: resolve => require(['./pages/product.vue', resolve])
+                    component: () =>
+                        import ('./pages/product.vue')
                 },
                 {
                     path: 'detail/:id',
                     name: 'detail',
                     //  component: Detail
-                    component: resolve => require(['./pages/detail.vue', resolve])
+                    // component: resolve => require(['./pages/detail.vue', resolve])
+                    component: () =>
+                        import ('./pages/detail.vue')
                 },
             ]
         },
@@ -41,37 +45,55 @@ export default new Router({
 
             path: '/cart',
             name: 'cart',
-            component: Cart,
+            //  component: Cart,
+            //   component: resolve => require(['./pages/cart.vue'], resolve)
+            component: () =>
+                import ('./pages/cart.vue')
         },
         {
             path: '/login',
             name: 'login',
             //component: Login
-            component: resolve => require(['./pages/login.vue'], resolve)
+            // component: resolve => require(['./pages/login.vue'], resolve)
+            component: () =>
+                import ('./pages/login.vue')
         },
         {
             path: '/order',
             name: 'order',
-            component: Order,
+            //component: Order,
+            component: () =>
+                import ('./pages/order.vue'),
             children: [{
                     path: 'list',
                     name: 'order-list',
-                    component: OrderList,
+                    //component: OrderList,
+                    //       component: resolve => require(['./pages/orderList.vue'], resolve)
+                    component: () =>
+                        import ('./pages/orderList.vue')
                 },
                 {
                     path: 'confirm',
                     name: 'order-confirm',
-                    component: OrderConfirm,
+                    // component: OrderConfirm,
+                    // component: resolve => require(['./pages/orderConfirm.vue'], resolve)
+                    component: () =>
+                        import ('./pages/orderConfirm.vue')
                 },
                 {
                     path: 'pay',
                     name: 'order-pay',
-                    component: OrderPay,
+                    // component: OrderPay,
+                    //  component: resolve => require(['./pages/orderPay.vue'], resolve)
+                    component: () =>
+                        import ('./pages/orderPay.vue')
                 },
                 {
                     path: 'alipay',
                     name: 'alipay',
-                    component: AliPay,
+                    //  component: AliPay,
+                    component: () =>
+                        import ('./pages/login.vue')
                 }
             ]
         }
