1dbe5afe5bbcabaa3ba60499498f49ede448bef1
lceihen <2750418124@qq.com>
2021-02-03 15:26:20 +0800

    alipay跳转路由改正

--- mimall/src/router.js
+++ mimall/src/router.js
@@ -14,88 +14,88 @@ import Index from './pages/index';
 Vue.use(Router);
 export default new Router({
     routes: [{
-            path: '/',
-            name: 'home',
-            component: Home,
-            redirect: '/index',
-            children: [{
-                    path: 'index',
-                    name: 'index',
-                    component: Index
-                },
-                {
-                    path: 'product/:id',
-                    name: 'product',
-                    //    component: Product,
-                    //  component: resolve => require(['./pages/product.vue', resolve])
-                    component: () =>
-                        import ('./pages/product.vue')
-                },
-                {
-                    path: 'detail/:id',
-                    name: 'detail',
-                    //  component: Detail
-                    // component: resolve => require(['./pages/detail.vue', resolve])
-                    component: () =>
-                        import ('./pages/detail.vue')
-                },
-            ]
+        path: '/',
+        name: 'home',
+        component: Home,
+        redirect: '/index',
+        children: [{
+            path: 'index',
+            name: 'index',
+            component: Index
         },
         {
+            path: 'product/:id',
+            name: 'product',
+            //    component: Product,
+            //  component: resolve => require(['./pages/product.vue', resolve])
+            component: () =>
+                import('./pages/product.vue')
+        },
+        {
+            path: 'detail/:id',
+            name: 'detail',
+            //  component: Detail
+            // component: resolve => require(['./pages/detail.vue', resolve])
+            component: () =>
+                import('./pages/detail.vue')
+        },
+        ]
+    },
+    {
 
-            path: '/cart',
-            name: 'cart',
-            //  component: Cart,
-            //   component: resolve => require(['./pages/cart.vue'], resolve)
+        path: '/cart',
+        name: 'cart',
+        //  component: Cart,
+        //   component: resolve => require(['./pages/cart.vue'], resolve)
+        component: () =>
+            import('./pages/cart.vue')
+    },
+    {
+        path: '/login',
+        name: 'login',
+        //component: Login
+        // component: resolve => require(['./pages/login.vue'], resolve)
+        component: () =>
+            import('./pages/login.vue')
+    },
+    {
+        path: '/order',
+        name: 'order',
+        //component: Order,
+        component: () =>
+            import('./pages/order.vue'),
+        children: [{
+            path: 'list',
+            name: 'order-list',
+            //component: OrderList,
+            //       component: resolve => require(['./pages/orderList.vue'], resolve)
             component: () =>
-                import ('./pages/cart.vue')
+                import('./pages/orderList.vue')
         },
         {
-            path: '/login',
-            name: 'login',
-            //component: Login
-            // component: resolve => require(['./pages/login.vue'], resolve)
+            path: 'confirm',
+            name: 'order-confirm',
+            // component: OrderConfirm,
+            // component: resolve => require(['./pages/orderConfirm.vue'], resolve)
             component: () =>
-                import ('./pages/login.vue')
+                import('./pages/orderConfirm.vue')
         },
         {
-            path: '/order',
-            name: 'order',
-            //component: Order,
+            path: 'pay',
+            name: 'order-pay',
+            // component: OrderPay,
+            //  component: resolve => require(['./pages/orderPay.vue'], resolve)
             component: () =>
-                import ('./pages/order.vue'),
-            children: [{
-                    path: 'list',
-                    name: 'order-list',
-                    //component: OrderList,
-                    //       component: resolve => require(['./pages/orderList.vue'], resolve)
-                    component: () =>
-                        import ('./pages/orderList.vue')
-                },
-                {
-                    path: 'confirm',
-                    name: 'order-confirm',
-                    // component: OrderConfirm,
-                    // component: resolve => require(['./pages/orderConfirm.vue'], resolve)
-                    component: () =>
-                        import ('./pages/orderConfirm.vue')
-                },
-                {
-                    path: 'pay',
-                    name: 'order-pay',
-                    // component: OrderPay,
-                    //  component: resolve => require(['./pages/orderPay.vue'], resolve)
-                    component: () =>
-                        import ('./pages/orderPay.vue')
-                },
-                {
-                    path: 'alipay',
-                    name: 'alipay',
-                    //  component: AliPay,
-                    component: () =>
-                        import ('./pages/login.vue')
-                }
-            ]
+                import('./pages/orderPay.vue')
+        },
+        {
+            path: 'alipay',
+            name: 'alipay',
+            //  component: AliPay,
+            component: () =>
+                import('./pages/alipay.vue')
         }
+        ]
+    }
     ]
 });
